e07aadddefb8591073f5f521166859471f5c7525
Nathan Carr <Nathan.J.Carr@student.uts.edu.au>
2018-05-30 22:00:51 +1000

    Fixing REST service; Adding .gitignore for build directory.

--- src/java/uts/wsd/Book.java
+++ src/java/uts/wsd/Book.java
@@ -5,8 +5,10 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name="book")
 public class Book implements Serializable {
         @XmlAttribute(name = "id")
         private String id;
